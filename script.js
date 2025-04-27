32e4cab878a9975f93fda6c81feb669bf7379563
Zaw Lin Naing <zawlinnaing@securelinkmm.com>
2025-04-27 08:37:21 +0630

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// FAQ toggle
+const faqQuestions = document.querySelectorAll('.faq-question');
+
+faqQuestions.forEach(question => {
+    question.addEventListener('click', () => {
+        const answer = question.nextElementSibling;
+        answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
+    });
+});
